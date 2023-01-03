1201ca6454ad2e229884f4eb44ab89e3e3c78374
Shibi Suriya <shibisuriya.work@gmail.com>
2023-01-03 15:26:05 +0530

    feat: added webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		main: path.resolve(__dirname, './src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Drag select marker',
+			template: path.resolve(__dirname, './src/template.html'), // template file
+			filename: 'index.html', // output file
+		}),
+	],
+};
